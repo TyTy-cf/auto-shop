0b9b276c75d8a9c24134a51e412caf7e723d73e5
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2021-02-16 11:47:17 +0100

    Added relations

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Categories
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Models::class, mappedBy="categories", orphanRemoval=true)
+     */
+    private $Models;
+
+    public function __construct()
+    {
+        $this->Models = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Categories
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Models[]
+     */
+    public function getModels(): Collection
+    {
+        return $this->Models;
+    }
+
+    public function addModel(Models $model): self
+    {
+        if (!$this->Models->contains($model)) {
+            $this->Models[] = $model;
+            $model->setCategories($this);
+        }
+
+        return $this;
+    }
+
+    public function removeModel(Models $model): self
+    {
+        if ($this->Models->removeElement($model)) {
+            // set the owning side to null (unless already changed)
+            if ($model->getCategories() === $this) {
+                $model->setCategories(null);
+            }
+        }
 
         return $this;
     }
