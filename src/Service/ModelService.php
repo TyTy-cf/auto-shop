7cf6fc4b912496d6ab3b077ba99e8afc37b287de
Kevin Tourret <tourret.kevin@gmail.com>
2021-02-16 14:52:35 +0100

    some more command and data

--- src/Service/ModelService.php
+++ src/Service/ModelService.php
@@ -10,7 +10,7 @@ use App\Repository\CategoryRepository;
 /**
  * Class ModelService.php
  *
- * @author Kevin Tourret
+ * @author Marine Bertin
  */
 class ModelService
 {
@@ -46,11 +46,109 @@ class ModelService
             'Astra' => 'Citadine',
             'Corsa' => 'Citadine',
             'Zafira' => 'Monospace',
+            'Meriva' => 'Monospace',
+            'Optimus Prime' => '4x4',
         ];
         $arrayModel['Audi'] = [
             'A5' => 'Berline',
-            'Corsa' => 'Citadine',
-            'Zafira' => 'Monospace',
+            'Batmobile' => 'Tank',
+            'Bumbo' => 'Mini',
+            'A3' => 'Berline',
+            'R8' => 'Coupée',
+        ];
+        $arrayModel['Ford'] = [
+            'Falcon' => 'Mad Max',
+            'Gran Torino' => 'Starsky et Hutch',
+            'KITT' => 'K2000',
+        ];
+        $arrayModel['WackyRaces'] = [
+            'Démone Double-Zero Grand Sport' => 'Citadine',
+            'Caraverne' => 'Break',
+            'Dingo-Limousine' => 'SUV',
+            'Auto-aero-fuseau-planeur' => 'Pick up',
+            'Bolide Ecarlate' => 'Pick up',
+            'Compact PussyCat' => 'Citadine',
+            'Tocard Tank' => 'Break',
+            'Turbo Terrific' => 'Berlines',
+            'Tacot Tout-Terrain' => 'Berlines'
+        ];
+        $arrayModel['Renault'] = [
+            'Clio' => 'Citadine',
+            'Kangoo' => 'Utilitaire',
+            'Scenic' => 'Monospace',
+            'Twingo' => 'Citadine',
+        ];
+        $arrayModel['CTR'] = [
+            'Trikee' => 'Kart',
+            'Retro Prime' => 'Kart',
+            'Yellow Horde' => 'Kart',
+            'Crikey' => 'Kart',
+            'Imperium' => 'Kart',
+            'The Deadinator' => 'Kart',
+            'Futurosity' => 'Kart',
+            'Probulot 2000' => 'Kart',
+            'Harm Aid' => 'Kart',
+            'Bad Girl' => 'Kart',
+            'The Ruckus' => 'Kart',
+            'Le Chaux' => 'Kart',
+            'The Guzzler' => 'Kart',
+            'Overcompensator' => 'Kart',
+            'Bone Machine' => 'Kart',
+            'Colostomator' => 'Kart',
+            'Nostalginator' => 'Kart',
+            'Bandibuggy' => 'Kart',
+            'Fairy Mobile' => 'Kart',
+            'Organ Grinder' => 'Kart',
+            'Tweenage Wasteland' => 'Kart',
+            'Angsterator' => 'Kart',
+            'Slave Driver' => 'Kart',
+            'Extreme Surplus' => 'Kart',
+            'Junkulon Prime' => 'Kart',
+            'Doom Buggy' => 'Kart',
+            'Zwergauto' => 'Kart',
+            'Der Pickle' => 'Kart',
+            'Pressurizer' => 'Kart',
+            'Toy Block Car' => 'Kart',
+            'CRster' => 'Kart'
+        ];
+        $arrayModel['Volkswagen'] = [
+            'Touran' => 'Monospace',
+            'Tiguan' => 'SUV',
+            'Polo' => 'Polyvalente',
+            'Golf' => 'Compacte',
+            'Coccinelle' => 'Compacte',
+            'Passat' => 'Familiale routière'
+        ];
+        $arrayModel['Toyota'] = [
+            'Yaris' => 'Polyvalente',
+            'Aygo' => 'Mini citadine',
+            'Prius' => 'Hybride',
+            'Supra' => 'Grand tourisme'
+        ];
+        $arrayModel['Ferrari'] = [
+            'Roma' => 'Coupée Sport',
+            '488' => 'Coupée Sport',
+            'Tributo' => 'Coupée Sport',
+            'T8 Spider' => 'Coupée Sport',
+            'Monza' => 'Coupée Sport',
+            'Stradale' => 'Coupée Sport',
+        ];
+        $arrayModel['Nissan'] = [
+            'GT-R' => 'Supercar',
+            'Qashqai' => 'Crossover',
+            'Juke' => 'Crossover urbain',
+            'Leaf' => 'Compacte'
+        ];
+        $arrayModel['Mercedes'] = [
+            'Classe A' => 'Berlines',
+            'Classe C' => 'Berlines',
+            'Classe E' => 'Berlines',
+            'Classe S' => 'Berlines',
+            'Classe S Limousine' => 'Berlines',
+            'Mercedes-Maybach Classe S' => 'Berlines',
+            'EQA' => 'SUV',
+            'Classe B' => 'Monospace',
+            'Classe V' => 'Monospace',
         ];
         return $arrayModel;
     }
