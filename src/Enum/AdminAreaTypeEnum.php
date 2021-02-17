eabd2454fc61c30fc08a511b2f90eded70d5dd3c
Théau Goncalves <theau26@gmail.com>
2021-02-17 11:10:47 +0100

    Import region method

---
+++ src/Enum/AdminAreaTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Enum;
+
+
+class AdminAreaTypeEnum
+{
+    const REGIONS = 'region';
+    const DEPARTMENT = 'department';
+    const CITY = 'city';
+}
--- src/Service/AdministrativeArea/AdminAreaImportService.php
+++ src/Service/AdministrativeArea/AdminAreaImportService.php
@@ -4,10 +4,56 @@
 namespace App\Service\AdministrativeArea;
 
 
+use App\Entity\AdministrativeArea;
+use App\Enum\AdminAreaTypeEnum;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class AdminAreaImportService
 {
+
+    private HttpClientInterface $client;
+    private EntityManagerInterface $em;
+
+    /**
+     * AdminAreaImportService constructor.
+     * @param HttpClientInterface $client
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em)
+    {
+        $this->client = $client;
+        $this->em = $em;
+    }
+
+
     public function importRegion()
     {
-        dump('coucou');
+        $response = $this->client->request(
+            'GET',
+            'https://geo.api.gouv.fr/regions',
+//            [
+//                'query' => [
+//                    'nom' => 'Hauts-de-France',
+//                ],
+//            ]
+        );
+
+        if($response->getStatusCode() === 200) {
+            $results = $response->toArray();
+
+            if(count($results) > 0) {
+                foreach ($results as $region) {
+                    $entity = new AdministrativeArea();
+                    $entity->setName($region['nom']);
+                    $entity->setCode($region['code']);
+                    $entity->setType(AdminAreaTypeEnum::REGIONS);
+
+                    $this->em->persist($entity);
+                }
+
+                $this->em->flush();
+            }
+        }
     }
 }
