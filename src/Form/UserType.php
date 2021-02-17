1174c488f0c06887d1f437b1ccc5d227b4b1532e
Théau Goncalves <theau26@gmail.com>
2021-02-17 14:22:27 +0100

    Password type

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,14 +27,13 @@ class UserType extends AbstractType
                             (
                                 'Yes' => 'ROLE_USER',
                             ),
-
-
                         )
                 ,
                     'multiple' => true,
                     'required' => true,
                 )
             )
+            ->add('password', PasswordType::class);
         ;
     }
 
