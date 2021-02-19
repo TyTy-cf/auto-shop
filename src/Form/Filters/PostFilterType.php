d85e73d122f8b4739e91bf00d758f84cb2886cf8
Théau Goncalves <theau26@gmail.com>
2021-02-19 16:06:34 +0100

    Date filter type (to scss)

--- src/Form/Filters/PostFilterType.php
+++ src/Form/Filters/PostFilterType.php
@@ -5,6 +5,9 @@ namespace App\Form\Filters;
 
 
 use Lexik\Bundle\FormFilterBundle\Filter\FilterOperands;
+use Lexik\Bundle\FormFilterBundle\Filter\Form\Type\DateFilterType;
+use Lexik\Bundle\FormFilterBundle\Filter\Form\Type\DateRangeFilterType;
+use Lexik\Bundle\FormFilterBundle\Filter\Form\Type\DateTimeRangeFilterType;
 use Lexik\Bundle\FormFilterBundle\Filter\Form\Type\TextFilterType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,15 +17,26 @@ class PostFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title', TextFilterType::class, [
-            'condition_pattern' => FilterOperands::STRING_CONTAINS,
-        ]);
+        $builder
+            ->add('title', TextFilterType::class, [
+                'condition_pattern' => FilterOperands::STRING_CONTAINS,
+            ])
+            ->add('createdAt', DateTimeRangeFilterType::class, [
+                'left_datetime_options' => array(
+                    'widget'    => 'single_text',
+                ),
+                'right_datetime_options' => array(
+
+                    'widget'    => 'single_text',
+                ),
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'csrf_protection'   => false,
+            'csrf_protection' => false,
             'validation_groups' => array('filtering'),
         ]);
     }
