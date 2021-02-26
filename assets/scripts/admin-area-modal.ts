2fe9c192bbc7053792c87cffa8fd5f684b387628
Goncalves Théau <theau26@gmail.com>
2021-02-26 16:01:22 +0100

    Modal bugged

--- assets/scripts/admin-area-modal.ts
+++ assets/scripts/admin-area-modal.ts
@@ -5,16 +5,55 @@ import {Modal} from 'bootstrap';
     const modal = document.querySelector('#admin-area-modal');
     const bsModal = new Modal(modal);
 
-
     adminAreaBtns.forEach((button: HTMLButtonElement) => {
         button.addEventListener('click', () => {
 
+            let url = '/api/administrative_areas/' + button.getAttribute('data-aa-id');
+            fetchAdminArea(url);
 
-
-            bsModal.show();
         });
     });
 
 
+    function fillModal(adminArea, parentArea = null)
+    {
+        let toFilledSection = modal.querySelector('.admin-area-info');
+        toFilledSection.querySelector('.aa-name').innerHTML =  adminArea.name;
+        toFilledSection.querySelector('.aa-code').innerHTML =  adminArea.code;
 
+
+        let $parentCode = toFilledSection.querySelector<HTMLSpanElement>('.aa-parent');
+        $parentCode.closest('li').className = '';
+
+        if(adminArea.parentCode == null) {
+            $parentCode.closest('li').classList.add('d-none');
+        } else {
+            toFilledSection.querySelector('.aa-parent').innerHTML =  parentArea.name;
+        }
+
+    }
+
+    function fetchAdminArea(url: string)
+    {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }).then((response: Response) => {
+
+            return response.json();
+        }).then((adminArea) => {
+
+            if(adminArea.parentCode !== null) {
+                fetchAdminArea('/api/administrative_areas/' + adminArea.parentCode)
+                fillModal(adminArea, parentArea);
+            } else {
+                fillModal(adminArea);
+            }
+        });
+
+        bsModal.show();
+    }
 })();
